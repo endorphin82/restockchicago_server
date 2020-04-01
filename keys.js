d3c2d6bfed3c2df5e5574f7350dfab479f0dd6d9
endorphin82 <endorphin82@gmail.com>
2020-04-01 19:41:39 +0300

    add api addProductsWithoutCategoryInTrash

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
 module.exports = {
-  MONGO_URL: 'mongodb://admin:123admin@ds329668.mlab.com:29668/restockchicago'
-  // MONGO_URL: 'mongodb://localhost:27017/restockchicago'
+  // MONGO_URL: 'mongodb://admin:123admin@ds329668.mlab.com:29668/restockchicago'
+  MONGO_URL: "mongodb://localhost:27017/restockchicago",
+  // local
+  TRASH_ID: "5e846688cd408b19cec669cf",
+
+  // TRASH_ID: '5e84ad3fe6b396aef7657ca4',
 }
 
 // mongoose.connect('mongodb://localhost:27017/restockchicago', {useNewUrlParser: true})
