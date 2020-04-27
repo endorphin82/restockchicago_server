af4ff9624d2bedf0a22654c460745497e84feabc
endorphin82 <endorphin82@gmail.com>
2020-04-27 19:56:03 +0300

    add productsByNameAndCategoriesId

--- schema/schema.js
+++ schema/schema.js
@@ -161,7 +161,7 @@ const Mutation = new GraphQLObjectType({
           name,
           images,
           icons,
-          parent
+          parent,
         })
         // console.log("###############", category)
 
@@ -214,7 +214,23 @@ const Query = new GraphQLObjectType({
       args: { name: { type: GraphQLString } },
       resolve(parent, { name }) {
         console.info("productByName:", name)
-        return Products.find({ name: { $regex: name, $options: "i" } }).sort({_id: -1})
+        return Products.find({ name: { $regex: name, $options: "i" } }).sort({
+          _id: -1,
+        })
+      },
+    },
+    productsByNameAndCategoriesId: {
+      type: new GraphQLList(ProductType),
+      args: {
+        name: { type: GraphQLString },
+        categories: { type: new GraphQLList(GraphQLString) },
+      },
+      resolve(parent, { name, categories }) {
+        console.info("productByName:", name, categories)
+        return Products.find({
+          name: { $regex: name, $options: "i" },
+          categories: { $in: categories }
+        }).sort({ _id: -1 })
       },
     },
     categoryById: {
@@ -231,7 +247,7 @@ const Query = new GraphQLObjectType({
       args: { parent: { type: GraphQLString } },
       resolve(_, { parent }) {
         console.info("categoryById:", parent)
-        return Categories.find({parent})
+        return Categories.find({ parent })
       },
     },
 
@@ -239,14 +255,14 @@ const Query = new GraphQLObjectType({
       type: new GraphQLList(ProductType),
       resolve: () => {
         console.info("productsAll")
-        return Products.find({}).sort({_id: -1})
+        return Products.find({}).sort({ _id: -1 })
       },
     },
     productsByCategoryId: {
       type: new GraphQLList(ProductType),
       args: { id: { type: GraphQLString } },
       resolve(parent, { id }) {
-        return Products.find({ categories: id }).sort({_id: -1})
+        return Products.find({ categories: id }).sort({ _id: -1 })
       },
     },
     categoryByName: {
@@ -261,7 +277,7 @@ const Query = new GraphQLObjectType({
       type: new GraphQLList(CategoryType),
       resolve: () => {
         console.info("categoriesAll")
-        return Categories.find({}).sort({createdAt: -1})
+        return Categories.find({}).sort({ createdAt: -1 })
       },
     },
     categoriesByListNames: {
